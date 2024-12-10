f23abbf464783385f0221bb97bfad43a7e75e154
Travis Gertz <trav@louderthanten.com>
2024-12-10 17:41:30 +0800

    Cleanup config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,16 +4,17 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.CRAFT_HOSTNAME || 'api.starter-next.ddev.site',
+        hostname: process.env.NEXT_PUBLIC_CRAFT_URL || 'api.starter-next.ddev.site',
         port: '',
         pathname: '/**',
       },
     ],
   },
   env: {
-    SITE_NAME: process.env.SITE_NAME || 'Next Starter',
-    CRAFT_URL: process.env.CRAFT_URL || 'https://api.starter-next.ddev.site',
-    BASE_URL: process.env.BASE_URL || 'https://starter-next.ddev.site'
+    SITE_NAME: process.env.NEXT_PUBLIC_SITE_NAME,
+    CRAFT_URL: process.env.NEXT_PUBLIC_CRAFT_URL,
+    BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    GRAPHQL_TOKEN: process.env.NEXT_PUBLIC_CRAFT_API_TOKEN
   },
   reactStrictMode: true,
   poweredByHeader: false,

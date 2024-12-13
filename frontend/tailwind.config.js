f9fc936d65edcf4b0366f54f458add03b7810c8f
Travis Gertz <trav@louderthanten.com>
2024-12-13 11:52:52 +0800

    Get tailwind working with CKEditor

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./backend/templates/**/*.twig",
   ],
   theme: {
     extend: {
@@ -15,5 +16,31 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
+    require('@tailwindcss/forms')
   ],
+  // Add closses used in the Twig templates in /backend/templates/_partials for CKEditor
+  safelist: [
+    {
+      pattern: /bg-slate-.+/,
+      variants: ['lg', 'md', 'sm']
+    },
+    {
+      pattern: /rounded-.+/,
+      variants: ['lg', 'md', 'sm']
+    },
+    {
+      pattern: /text-.+/,
+      variants: ['lg', 'md', 'sm']
+    },
+    {
+      pattern: /my-.+/,
+      variants: ['lg', 'md', 'sm']
+    },
+    {
+      pattern: /p-.+/,
+      variants: ['lg', 'md', 'sm']
+    },
+    'aspect-w-16',
+    'aspect-h-9'
+  ]
 };

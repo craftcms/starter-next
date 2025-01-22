8e9d809148254a975bc8968975e839f3567e6d1a
Travis Gertz <trav@louderthanten.com>
2025-01-22 09:51:09 +0700

    Fix image paths

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [process.env.CRAFT_URL],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.CRAFT_URL,
-        port: '',
-        pathname: '/**'
+        hostname: process.env.CRAFT_DOMAIN,
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: process.env.CRAFT_DOMAIN,
+        pathname: '/**',
       }
     ]
   },
   env: {
-    // We want variables from the `.env` file to be included in the built app:
-    // https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#bundling-environment-variables-for-the-browser
     SITE_NAME: process.env.SITE_NAME,
     CRAFT_URL: process.env.CRAFT_URL,
     BASE_URL: process.env.BASE_URL,

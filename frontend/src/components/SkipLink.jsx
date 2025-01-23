afc25b4c5556187977e962343b5780ebf0571483
Lupe Camacho <lupecamacho@Lupes-MacBook-Pro.local>
2025-01-23 10:02:28 -0800

    Update skip link styles to show on focus

--- frontend/src/components/SkipLink.jsx
+++ frontend/src/components/SkipLink.jsx
@@ -4,7 +4,7 @@ export function SkipLink() {
   return (
     <a
       href="#main"
-      className="sr-only"
+      className="skip-link absolute rounded font-bold bg-red-600 text-slate-50 px-6 py-4"
     >
       Skip to main content
     </a>

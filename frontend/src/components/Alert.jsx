7498a321470afc1d8da99d1e1e2546dec4d457bd
Travis Gertz <trav@louderthanten.com>
2025-01-23 17:46:21 +0700

    Cleanup Flashes

--- frontend/src/components/Alert.jsx
+++ frontend/src/components/Alert.jsx
@@ -5,25 +5,22 @@ import { useFlashes } from '../lib/flashes'
 export function Alert() {
   const { flashes } = useFlashes()
 
-  if (flashes.length === 0) return null
+  if (!flashes.length) return null
 
   return (
-    <div 
-      role="alert"
-      className="absolute top-0 left-0 right-0 z-50"
-    >
-      {flashes.map((flash) => (
+    <div role="alert" className="absolute top-0 left-0 right-0 z-50">
+      {flashes.map(({ id, message, level }) => (
         <p 
-          key={flash.id}
+          key={id}
           className={`
             container mx-auto p-2 text-md text-center text-slate-50
-            ${flash.level === 'error' ? 'bg-red-600' : ''}
-            ${flash.level === 'success' ? 'bg-emerald-600' : ''}
-            ${flash.level === 'warning' ? 'bg-yellow-500' : ''}
-            ${flash.level === 'info' ? 'bg-blue-600' : ''}
+            ${level === 'error' ? 'bg-red-600' : ''}
+            ${level === 'success' ? 'bg-emerald-600' : ''}
+            ${level === 'warning' ? 'bg-yellow-500' : ''}
+            ${level === 'info' ? 'bg-blue-600' : ''}
           `}
         >
-          {flash.message}
+          {message}
         </p>
       ))}
     </div>
